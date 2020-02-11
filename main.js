558b3c1d129df32c7ab166f2298857fdc97dfeef
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-02-11 08:47:05 +0100

    Top layer rotation without animation complete

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ const colors = {
     white: new THREE.Color(0.8,0.8,0.8), //0xffffff,
 }
 
-const size = 0.5
-
 /*
     Faces:
     A: 0, 1: Höger sida
@@ -31,6 +29,9 @@ const size = 0.5
     y: up is more, down is less, origo middle of cube
     z: towards you is more, away from you is less, origo middle of cube
 */
+
+var pivot = new THREE.Object3D();
+scene.add(pivot);
 
 const rubiks = [
     //Yellow side
@@ -67,13 +68,8 @@ const rubiks = [
     { x: 1, y: -1, z: 1, bottom: 'white', front: 'green', right: 'orange' },
     { x: -1, y: -1, z: -1, bottom: 'white', left: 'red', back: 'blue' },
     { x: 1, y: -1, z: -1, bottom: 'white', right: 'orange', back: 'blue' },
-]
-
-var pivot = new THREE.Object3D();
-scene.add(pivot);
-
-rubiks.forEach((data, idx) => {
-    const geometry = new THREE.BoxGeometry(size, size, size);
+].map((data, idx) => {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
     
     if (data.right !== undefined) {
         geometry.faces[0].color = colors[data.right]
@@ -106,14 +102,16 @@ rubiks.forEach((data, idx) => {
     }
     const material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors } );
     cube = new THREE.Mesh( geometry, material );
-    cube.position.x = data.x * size
-    cube.position.y = data.y * size
-    cube.position.z = data.z * size
+    cube.position.x = data.x
+    cube.position.y = data.y
+    cube.position.z = data.z
 
     pivot.add(cube);
+
+    return {...data, cube}
 })
 
-camera.position.z = 5;
+camera.position.z = 6;
 
 const rotate = [0,0]
 const speed = 0.03
@@ -131,6 +129,12 @@ window.addEventListener('keydown', (e) => {
     if (e.keyCode === 40) {
         rotate[0] = speed
     }
+    if (e.keyCode === 70) {
+        rotateCube('front')
+    }
+    if (e.keyCode === 84) {
+        rotateCube('top')
+    }
 })
 
 window.addEventListener('keyup', (e) => {
@@ -141,6 +145,51 @@ window.addEventListener('keyup', (e) => {
         rotate[0] = 0
     }
 })
+
+function cycle(input) {
+    // corners
+    if (input[0] === 1 && input[1] === -1) return [1,1]
+    if (input[0] === 1 && input[1] === 1) return [-1,1]
+    if (input[0] === -1 && input[1] === 1) return [-1,-1]
+    if (input[0] === -1 && input[1] === -1) return [1,-1]
+
+    // middles
+    if (input[0] === 1 && input[1] === 0) return [0,1]
+    if (input[0] === 0 && input[1] === 1) return [-1,0]
+    if (input[0] === -1 && input[1] === 0) return [0,-1]
+    if (input[0] === 0 && input[1] === -1) return [1, 0]
+}
+
+function rotateTopCornersClockwise(data) {
+    const [x ,z] = cycle([data.cube.position.x, data.cube.position.z])
+    data.cube.position.x = x
+    data.cube.position.z = z
+
+    data.cube.rotation.y += -Math.PI / 2
+}
+
+function rotateCube(side) {
+    if (side === 'top') {
+        rubiks.filter(d => d.cube.position.y === 1 && !(d.cube.position.x === 0 && d.cube.position.z === 0))
+            .forEach(rotateTopCornersClockwise)
+
+        /*let d
+        d = rubiks.find(d => d.x === 1 && d.y === 1 && d.z === 1)
+        if (d) {
+            d.x = -1
+            const cube = d.cube
+            cube.position.x = -1
+            cube.rotation.y += -Math.PI / 2
+        }
+        d = rubiks.find(d => d.x === 1 && d.y === 1 && d.z === -1)
+        if (d) {
+            d.z = 1
+            const cube = d.cube
+            cube.position.z = 1
+            cube.rotation.y += -Math.PI / 2
+        }*/
+    }
+}
 
 function animate() {
 	requestAnimationFrame( animate );
