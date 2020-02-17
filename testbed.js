f1b3a69578b54dd529c439a4c8a4aa6d370fbc35
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-02-17 10:22:28 +0100

    Net solves all cases but never without knowledge of the final test

--- testbed.js
+++ testbed.js
@@ -1,100 +1,78 @@
 const brain = require('./brain-browser.min.js')
 
-// Trainingdata with full string as output
-/*
 const trainingData = [
 	{
-		input: [0, 0, 0, 0],
-		output: { "": 1 }
-	},
-	{
 		input: [1, 1, 0, 0],
-		output: { "AB": 1 }
+		output: { A: 1 }
 	},
 	{
 		input: [0, 0, 1, 1],
-		output: { "AAAB": 1 }
+		output: { A: 1 }
 	},
 	{
 		input: [1, 0, 1, 0],
-		output: { "BAB": 1 }
+		output: { A: 1 }
 	},
 	{
 		input: [0, 1, 0, 1],
-		output: { "BAAAB": 1 }
+		output: { B: 1 }
+	},
+	{
+		input: [1, 1, 1, 1],
+		output: { B: 1 }
 	}
 ]
-*/
 
-// Trainingdata array output
-const trainingData = [
-	{
-		input: [0, 0, 0, 0],
-		output: ["-", "-", "-", "-"]
-	},
-	{
-		input: [1, 1, 0, 0],
-		output: ["A", "B", "-", "-"]
-	},
-	{
-		input: [0, 0, 1, 1],
-		output: ["A", "A", "A", "B"]
-	},
-	{
-		input: [1, 0, 1, 0],
-		output: ["A", "A", "B", "-"]
-	},
-	{
-		input: [0, 1, 0, 1],
-		output: ["B", "-", "-", "-"]
-	}
-].map(convertOutputKey(convertToFloat))
-
-const testingData = trainingData.concat({
-	input: [1, 1, 1, 1],
-	output: ["B", "A", "A", "B"].map(convertToFloat)
-})
-
-function convertOutputKey(func) {
-	return obj => {
-		return {
-			...obj,
-			output: obj.output.map(func)
-		}
-	}
-}
-
-function convertToFloat(input) {
-	if (input === "-") return 0
-	if (input === "A") return 0.5
-	if (input === "B") return 1
-}
-
-function approximate(fl) {
-	if (fl < 0.1) return "-"
-	if (fl > 0.1 && fl < 0.85) return "A"
-	if (fl > 0.85) return "B"
-
-	console.error("Should not be able to happen")
-}
+const testingData = trainingData
+/*.concat({
+	input: [1, 1, 1, 1]
+})*/
 
 console.log('Training data:')
 console.log(trainingData)
 
-function pickHighest(data) {
-    let bestScore = 0
-    let bestRotation = ''
-    for (rotation in data) {
-        if (data[rotation] > bestScore) {
-            bestScore = data[rotation]
-            bestRotation = rotation
-        }
-    }
-    return bestRotation
+function invert(x) {
+	return x === 0 ? 1 : 0
+}
+
+function move(type, data) {
+	if (type === "A") {
+		return [data[2], data[0], data[3], data[1]]
+	}
+	if (type === "B") {
+		return [data[0], invert(data[1]), data[2], invert(data[3])]
+	}
 }
 
 function testNetAgainstEntry(net) {
 	return (entry, idx) => {
+
+		console.log(`Testing starting position ${idx} - ${entry.input}`)
+
+		let done = false
+		let puzzle = entry.input.map(x => x)
+
+		new Array(4).fill().forEach(() => {
+			if (done) return
+			console.log(`Puzzle is at ${ puzzle }`)
+			const answer = brain.likely(puzzle, net)
+			puzzle = move(answer, puzzle)
+			console.log(`Net answer: ${ answer }`)
+			console.log(net.run(puzzle))
+
+			if (JSON.stringify(puzzle) === JSON.stringify([0,0,0,0])) {
+				if (done === false) {
+					console.log('Solution found')
+				}
+				done = true
+			}
+		})
+
+		console.log(`\n\n`)
+
+		return done
+
+		/*
 		const result = net.run(entry.input)
 		const approximated = Array.prototype.slice.call(result).map(approximate)
 		const isCorrect = JSON.stringify(approximated) === JSON.stringify(entry.output.map(approximate))
@@ -103,6 +81,7 @@ function testNetAgainstEntry(net) {
 		console.log('Answer:', approximated, `[${Array.prototype.slice.call(result).map(x => x.toFixed(5)).join(", ")}]`, `(${isCorrect ? 'correct' : 'incorrect'})`)
 		console.log("\n")
 		return isCorrect
+		*/
 	}
 }
 
@@ -114,12 +93,12 @@ let iteration = 0
 let bestTotal = 0
 let totalScore = 0
 let bestNet
-let totalIterations = 50000
+let totalIterations = 100
 
 while (iteration++ < totalIterations) {
 
 	const config = {
-		hiddenLayers: new Array(rand(5)).fill().map(() => rand(20)),
+		//hiddenLayers: new Array(rand(5)).fill().map(() => rand(20)),
 	}
 
 	const net = new brain.NeuralNetwork(config)
@@ -138,9 +117,10 @@ while (iteration++ < totalIterations) {
 	totalScore += successes
 
 	console.log(`\n\n`)
-	console.log(`Current best: ${bestTotal} ${iteration}/${totalIterations}`)
+	console.log(`Current best: ${bestTotal}/${testingData.length}`)
+	console.log(`Iteration: ${iteration}/${totalIterations}`)
 }
 
-console.log(`\n\n\n\n\n\n\n`)
-console.log(`Average`, `${(totalScore / totalIterations).toFixed(2)}%`)
+console.log(`\n\n\n\n`)
+//console.log(`Average`, `${(totalScore / totalIterations).toFixed(2)}%`)
 console.log(`Best net:`, `${bestTotal}/${testingData.length} ${(bestTotal / testingData.length * 100).toFixed(2)}%`)
