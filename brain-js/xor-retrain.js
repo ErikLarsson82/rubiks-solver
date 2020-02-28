c63b5980eb9f80702c91ccdce5237348c59de5a2
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-02-28 18:59:12 +0100

    More XOR retrain knowledge

--- brain-js/xor-retrain.js
+++ brain-js/xor-retrain.js
@@ -1,45 +1,32 @@
-const brain = require('../brain-browser.min.js')
+const brain = require('../brain-browser.js')
 
-const net = new brain.NeuralNetwork()
-
-const config = {
-  //iterations: 100,
-}
+let net = new brain.NeuralNetwork()
 
 const data = [
   { input: [0], output: { "A": 1 } },
-  { input: [0], output: { "A": 0 } },
   { input: [1], output: { "B": 1 } }
 ]
 
-const result = net.train(data, config)
+const result = net.train(data)
 
 console.log('\nFirst training')
 console.log(net.run([0]))
 console.log(net.run([1]))
-console.log(`0 - ${brain.likely([0], net)}`)
-console.log(`1 - ${brain.likely([1], net)}`)
 
 console.log(result)
 
-/*console.log(net.train([
-  { input: [1], output: { "A": 1 } },
-  //{ input: [0], output: { "B": 1 } }
-]))
+// { retrain: false/true } seems not to have effect (its only for FeedForward), but creating new net resets it
+//net = new brain.NeuralNetwork()
 
-console.log('\nSecond training')
+const data2 = [
+  { input: [0], output: { "A": 0 } },
+  { input: [1], output: { "B": 0 } }
+]
+const res2 = net.train(data2 /*, { reinforce: false }*/)
+
+
+console.log('\nSecond training - notice big difference between A and B - without retraining that doesnt happen')
 console.log(net.run([0]))
 console.log(net.run([1]))
-console.log(brain.likely([0], net))
-console.log(brain.likely([1], net))*/
 
-
-function agentIndexToBinary(idx) {
-	const floatme = x => x === "1" ? 1.0 : 0.0
-	return leftPad("0000", idx.toString(2)).split('').map(floatme)
-}
-
-function leftPad(template, str) {
-	const full = template.concat(str)
-	return full.substr(str.length)
-}
+console.log(res2)
