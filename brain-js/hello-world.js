13e7ccc8f5dc51e354c7d459758d7491e01eeb9e
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-02-28 12:36:19 +0100

    Testing different inputs - should always be binary - integers only fail

--- brain-js/hello-world.js
+++ brain-js/hello-world.js
@@ -1,4 +1,4 @@
-const brain = require('../brain-browser-min.js')
+const brain = require('../brain-browser.min.js')
 
 const config = {
   binaryThresh: 0.5,
@@ -8,16 +8,57 @@ const config = {
 }
 
 // create a simple feed forward neural network with backpropagation
-const net = new brain.NeuralNetwork(config)
+const net = new brain.NeuralNetwork()
 
+// This works
 net.train([
-  { input: [-10, -10], output: { "A": 1 } },
-  { input: [-10, 5], output: { "B": 1 } },
-  { input: [5, -10], output: { "B": 1 } },
-  { input: [5, 5], output: { "A": 1 } },
+  { input: [0,0], output: { "A": 1 } },
+  { input: [0,1], output: { "B": 1 } },
+  { input: [1,0], output: { "C": 1 } },
+  { input: [1,1], output: { "D": 1 } },
 ])
 
-console.log(net.run([-10, -10]))
-console.log(net.run([-10, 5]))
-console.log(net.run([5, -10]))
-console.log(net.run([5, 5]))
+console.log(net.run([0,0]))
+console.log(net.run([0,1]))
+console.log(net.run([1,0]))
+console.log(net.run([1,1]))
+
+// This does not work
+/*
+net.train([
+  { input: 0, output: { "A": 1 } },
+  { input: 1, output: { "B": 1 } },
+  { input: 2, output: { "C": 1 } },
+  { input: 3, output: { "D": 1 } },
+])
+
+console.log(net.run(0))
+console.log(net.run(1))
+console.log(net.run(2))
+console.log(net.run(3))
+*/
+
+// Attempt - this works
+/*
+net.train([
+  { input: agentIndexToBinary(0), output: { "A": 1 } },
+  { input: agentIndexToBinary(1), output: { "B": 1 } },
+  { input: agentIndexToBinary(2), output: { "C": 1 } },
+  { input: agentIndexToBinary(3), output: { "D": 1 } },
+])
+
+console.log(net.run(agentIndexToBinary(0)))
+console.log(net.run(agentIndexToBinary(1)))
+console.log(net.run(agentIndexToBinary(2)))
+console.log(net.run(agentIndexToBinary(3)))
+*/
+
+function agentIndexToBinary(idx) {
+	const floatme = x => x === "1" ? 1.0 : 0.0
+	return leftPad("0000", idx.toString(2)).split('').map(floatme)
+}
+
+function leftPad(template, str) {
+	const full = template.concat(str)
+	return full.substr(str.length)
+}
